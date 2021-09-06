0e692a2ccede7c6aa42b239f0d200cf861cdd460
katerz avocado <ktrinaapodaca@yahoo.com>
2021-09-06 04:11:38 -0600

    updating server.js

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,23 @@ CREATE DATABASE employee_db;
 
 USE employee_db; 
 
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee( 
     id NOT NULL INT AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL, 
-    role_id INT NOT NULL INDEX role_ind (role_id) CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE, 
+    role_id INT NOT NULL INDEX role_ind (role_id) CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES Emrole(id) ON DELETE CASCADE, 
     manager_id INT INDEX manager_ind (manager_id) CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
-) 
-CREATE TABLE role( 
+);
+DROP TABLE IF EXISTS Emrole; 
+CREATE TABLE Emrole( 
     id NOT NULL INT AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(30) NOT NULL UNIQUE, 
     salary NOT NULL DECIMAL, 
     department_id NOT NULL INT INDEX department_ind (department_id) CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
-)
-
+);
+DROP TABLE IF EXISTS department; 
 CREATE TABLE department(
     id NOT NULL INT AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(30) NOT NULL UNIQUE 
-)
+);
